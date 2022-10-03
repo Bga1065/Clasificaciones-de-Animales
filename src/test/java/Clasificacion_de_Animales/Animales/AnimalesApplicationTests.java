8524acc24130ae2568e4180e23ab0de67c267093
Bga1065 <106782405+Bga1065@users.noreply.github.com>
2022-10-02 22:07:43 -0600

    Semana 04, Practica 02

---
+++ src/test/java/Clasificacion_de_Animales/Animales/AnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package Clasificacion_de_Animales.Animales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
